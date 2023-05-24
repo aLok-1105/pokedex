dadd507d01d2a73c4a7732e4496b7c44a247ffa3
aLok-1105 <alokranjan11052003@gmail.com>
2023-05-24 13:58:34 +0530

    updated-search-feat

--- src/components/PokeGrid.js
+++ src/components/PokeGrid.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import PokeCard from './PokeCard';
 
-export default function PokeGrid({pokemon, input, allPokemon}) {
+export default function PokeGrid({pokemon, input}) {
     // console.log(allPokemon);
 
     
   return (
     <div className='container poke-container'>
-            {pokemon.filter((item)=>{
-                return input.toLowerCase() ===''? item : item.name.toLowerCase().includes(input);
-            }).map((item)=>(
-                <PokeCard key={item.id} img={item.image} name={item.name} />
+            {pokemon.filter((pokemon)=>{
+                return input.toLowerCase() ===''? pokemon : pokemon.name.toLowerCase().includes(input);
+            }).map((pokemon)=>(
+                <PokeCard key={pokemon.id} img={pokemon.image} name={pokemon.name} />
             ))}
         
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,75 +2,24 @@
 
 import React, { useEffect, useState } from 'react';
 import PokeGrid from '../components/PokeGrid';
-import SearchBar from '../components/SearchBar';
-import PokeCard from '../components/PokeCard';
-
 
 export default function Home() {
-	var [input, setInput] = useState('');
-
-	const handleChange = (event) => {
-		setInput(event.target.value);
-	};
-
+	const [currentPage, setCurrentPage] = useState(1);
+	const [input, setinput] = useState('');
+	const pokemonsPerPage = 12;
 	const [pokemon, setPokemon] = useState([]);
-	const [allPokemon, setAllPokemon] = useState([]);
 
-  const [index, setIndex] = useState(12);
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 
-
-  const loadMore = ()=>{
-    setIndex(index+12);
-  }
-
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-// const getAllPokemon = ()=>{
-// 	fetch(`https://pokeapi.co/api/v2/pokemon/${input}`).then((res)=>res.json()).then((resu)=>{
-// 		console.log(resu);
-// 		setAllPokemon(resu);
-// 	})
-// }
-
-
-useEffect(()=>{
-
-
-const fetchAllPokemon = () => {
-  const promises = [];
-  for (let i = 1; i <= 40; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
-  }
-  Promise.all(promises).then((results) => {
-    const fetchedPokemon = results.map((result) => ({
-      name: capitalizeFirstLetter(result.name),
-      image: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${
-        result.id < 10
-          ? '00' + result.id
-          : '0' + result.id
-      }.png`,
-      type: result.types
-        .map((type) => type.type.name)
-        .join(', '),
-      id: result.id,
-    }));
-    // console.log(fetchedPokemon);
-    setAllPokemon(fetchedPokemon);
-  });
-};
-console.log(allPokemon);
-fetchAllPokemon();
-}, []);
-
-
+	const indexOfLastpokemon = currentPage * pokemonsPerPage;
+	const indexOfFirstpokemon = indexOfLastpokemon - pokemonsPerPage;
 
 	useEffect(() => {
 		const fetchPokemon = () => {
 			const promises = [];
-			for (let i = 1; i <= 20; i++) {
+			for (let i = 1; i <= 600; i++) {
 				const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 				promises.push(fetch(url).then((res) => res.json()));
 			}
@@ -80,7 +29,9 @@ fetchAllPokemon();
 					image: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${
 						result.id < 10
 							? '00' + result.id
-							: '0' + result.id
+							: result.id < 100
+							? '0' + result.id
+							: +result.id
 					}.png`,
 					type: result.types
 						.map((type) => type.type.name)
@@ -91,74 +42,60 @@ fetchAllPokemon();
 				setPokemon(fetchedPokemon);
 			});
 		};
-    // console.log(pokemon);
+		// console.log(pokemon);
 		fetchPokemon();
 	}, []);
 
-	const [currentPage, setCurrentPage] = useState(1);
-	const [searchTerm, setSearchTerm] = useState('');
-	const itemsPerPage = 10;
+	const filteredpokemons = pokemon.filter((pokemon) =>
+		pokemon.name.toLowerCase().includes(input.toLowerCase())
+	);
 
-	const filteredItems = pokemon.filter((item) =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  
-	// Calculate the indexes of the items to display on the current page
-	const indexOfLastItem = currentPage * itemsPerPage;
-	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-	const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
+	const currentpokemons = filteredpokemons.slice(
+		indexOfFirstpokemon,
+		indexOfLastpokemon
+	);
 
 	useEffect(() => {
 		setCurrentPage(1);
-	  }, [searchTerm]);
-  
-	// Handle page change
+	}, [input]);
+
 	const handlePageChange = (pageNumber) => {
-	  setCurrentPage(pageNumber);
+		setCurrentPage(pageNumber);
 	};
 
 	return (
 		<>
-		{/* <SearchBar data={allPokemon}/> */}
 			<div>
 				<div className='container mt-5'>
 					<input
 						type='text'
-						onChange={handleChange}
 						value={input}
+						onChange={(e) => setinput(e.target.value)}
+						placeholder='Search by name'
 					/>
 				</div>
 				<div>
-					<PokeGrid pokemon={pokemon} allPokemon={allPokemon} input={input} />
+					<PokeGrid pokemon={currentpokemons} input={input} />
 				</div>
-        <div>
-          <button type='button' onClick={loadMore}>Load More</button>
-        </div>
-		<div>
-
-		<input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search by name"
-      />
-
-		{currentItems.filter((item)=>{
-                return input.toLowerCase() ===''? item : item.name.toLowerCase().includes(input);
-            }).map((item)=>(
-                <PokeCard key={item.id} img={item.image} name={item.name} />
-            ))}
-
-		{/* {currentItems.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))} */}
-
-		{Array.from({ length: Math.ceil(filteredItems.length / itemsPerPage) }, (_, index) => (
-          <button key={index} onClick={() => handlePageChange(index + 1)}>
-            {index + 1}
-          </button>
-        ))}
-		</div>
+				<div>
+					{Array.from(
+						{
+							length: Math.ceil(
+								filteredpokemons.length /
+									pokemonsPerPage
+							),
+						},
+						(_, index) => (
+							<button
+								key={index}
+								onClick={() =>
+									handlePageChange(index + 1)
+								}>
+								{index + 1}
+							</button>
+						)
+					)}
+				</div>
 			</div>
 		</>
 	);
